6a5558554f65e3683c6fe350d235d04324a38538
Ch33m0 <callum.g.pollock@gmail.com>
2018-12-02 14:16:55 -0500

    Update test2.c

--- test2.c
+++ test2.c
@@ -4,7 +4,7 @@
 int main(){
   int c=khp("localhost",5001);                         //connect
  // k(c,"heyo:([]name:`symbol$(); num:`float$(); timeF:`time$())",(K)0);//insert
-    k(c,"heyo:([]name:`symbol$(); num:`int$())",(K)0);//insert
+    k(c,"heyo:([]name:`symbol$(); num:`int$())",(K)0);
 //  k(c,"heyo:([]name:`Time`Ross; num:22 13)",(K)0);//insert
 //  k(c,"save `heyo",(K)0);//insert
   int i;
@@ -12,12 +12,16 @@ int main(){
 //	K row= knk(3, ks((S) "NewGuy"), kf(20.00), kt(100000));
 	K row= knk(2, ks((S) "NewGuy"), ki(i));
 //	k(c, "`heyo insert (`niggerJim; 22)", (K)0);
-      k(c, "{[x] `heyo insert x}", row, (K) 0);
+	k(c, ".u.upd", ks((S) "heyo"), row, (K) 0);
+//	  k(c, "insert", ks((S) "heyo"), row, (K) 0); //try this one second
+     // k(c, "{[x] `heyo insert x}", row, (K) 0);
 	//k(c,"",(K)0); // flush 
+	  r0(c);
 	printf("done: %d", i);                             
 	  
 }
-k(c, "save `heyo", (K)0);
-k(c, "", (K)0);
+//k(c, "save `heyo", (K)0); //try without saving??
+//k(c, "", (K)0);
+kclose(c);
 printf("done, press enter to finish"); getchar();
 }
